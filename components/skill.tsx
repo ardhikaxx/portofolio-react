cd3f868155f86af64988d798555bcdfa71779f57
ardhikaxx <ardhikayanuar58@gmail.com>
2024-07-03 23:29:05 +0700

    update- view web

--- components/skill.tsx
+++ components/skill.tsx
@@ -21,7 +21,7 @@ export default function Features() {
       <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
       <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
 
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6 mb-10">
+      <div className="relative max-w-6xl mx-auto px-4 mt-5 sm:px-6 mb-10">
         <div className="pt-12 md:pt-20">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h2 mb-4">My Skill</h1>
