3deae5acdbc4b7cda181be6a597cd5c12fca1a5c
ardhikaxx <ardhikayanuar58@gmail.com>
2024-07-05 07:44:24 +0700

    update- view web

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,13 +21,15 @@ export default function Hero() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 mt-10 md:pb-20">
           <div className="text-center pb-12 md:pb-16 relative z-10">
-            <div className="flex items-center justify-center gap-2" data-aos="zoom-y-out">
+            <div className="flex flex-col md:flex-row items-center justify-center gap-2" data-aos="zoom-y-out">
               <Image className="inline -mt-4" src={LogoImage} width={100} height={100} alt="Logo" />
-              <h1 className="text-3xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4 flex items-center">Hello, I'm <span className="bg-clip-text ml-3 text-transparent bg-gradient-to-r from-[#4895D1] to-[#78B1E5]">Yanuar Ardhika</span></h1>
+              <h1 className="text-2xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4 flex items-center text-center">
+                Hello, I'm <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#4895D1] to-[#78B1E5] ml-3">Yanuar Ardhika</span>
+              </h1>
             </div>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">I am a Junior Web & Mobile Dev committed to improving user experience by creating responsive, visually appealing, and functional interfaces across multiple devices and platforms.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none flex justify-center gap-5 " data-aos="zoom-y-out" data-aos-delay="300">
+              <div className="max-w-xl mx-auto sm:max-w-none flex items-center justify-center gap-5" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 py-3 px-4 sm:py-3 sm:px-6 text-xs sm:text-base" href="https://ardhika-portofolio.vercel.app/" target='_blank' rel="noopener noreferrer">About Me</a>
                 </div>
