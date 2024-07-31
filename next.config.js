dfce3eab4acf3881f6b0c4cc4cd137e4a440ce3e
ardhikaxx <ardhikayanuar58@gmail.com>
2024-07-31 23:28:45 +0700

    update-

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "api.microlink.io",
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.microlink.io',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
