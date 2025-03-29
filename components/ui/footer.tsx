40aa19a0b8f285679a71dc76cb5483aa1ddfaa81
ardhikaxx <ardhikayanuar58@gmail.com>
2025-03-29 20:28:04 +0700

    update-

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
 
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
-              <a href="https://www.instagram.com/ardhikkaaa_/" target='_blank' className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out p-2" aria-label="Instagram">
+              <a href="https://www.instagram.com/ardhxkaa_/" target='_blank' className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out p-2" aria-label="Instagram">
                 <FontAwesomeIcon icon={faInstagram} className="w-5 h-5 fill-current" />
               </a>
             </li>
