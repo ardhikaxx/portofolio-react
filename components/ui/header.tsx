ac65d50501af08b4a03b7adc71393786d5e7c5b6
ardhikaxx <ardhikayanuar58@gmail.com>
2025-07-09 08:19:53 +0700

    update view

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -60,7 +60,7 @@ export default function Header() {
   }, [])
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${scrolled ? 'bg-white/30 backdrop-blur-sm shadow-lg' : ''}`}>
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${scrolled ? 'dark:bg-gray-900/30 backdrop-blur-sm shadow-lg' : ''}`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           <div className="shrink-0 mr-4">
